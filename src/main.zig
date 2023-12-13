e19d1f1c2b824192a49bb722f31c392ddfb6b7dc
Little Bear King <ryanburkle95@gmail.com>
2023-12-13 02:27:32 -0600

    add funtionality to 2 and 4 in the main menu

--- src/main.zig
+++ src/main.zig
@@ -11,29 +11,37 @@ const rand = @import("std").rand;
 const Allocator = std.mem.Allocator;
 const utils = @import("utils.zig");
 const Space = @import("Space.zig");
+const GalaxyGraph = @import("graph.zig").GalaxyGraph;
 const Star = Space.Star;
 
 const numStars: u32 = 10001;
 
 pub fn main() !void {
+
+    // Create the GalaxyGraph
+    // Create A Star
+    // Add Star to the Graph
+    // Have all Stars Created
+    // Create the Edges
+    // How do I make the edges based off of distance?
+    // Start the Shell
+
     // Create Allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const Alloc = gpa.allocator();
 
-    // create hasmap for Star Generation
-    var my_hash_map = std.StringHashMap(Star).init(Alloc);
-    defer my_hash_map.deinit();
-
+    // create the Galaxy to strore the Stars
+    var Galaxy = GalaxyGraph.init(Alloc);
     //Generate the Stars
-    try generateStars(&my_hash_map, Alloc);
+    try generateStars(&Galaxy.nodes, Alloc);
 
     // Start the Shell
-    try shiptermShell(Alloc);
+    try shiptermShell(Alloc, Galaxy);
 
     // Cleanup Hashmap after the program
     for (1..numStars) |value| {
         const getKey = try std.fmt.allocPrint(Alloc, "Star{}", .{value});
-        const getStar = my_hash_map.getPtr(getKey);
+        const getStar = Galaxy.nodes.getPtr(getKey);
         try getStar.?.deinit();
     }
 }
@@ -51,7 +59,7 @@ fn generateStars(self: *std.StringHashMap(Star), alloc: std.mem.Allocator) !void
     }
 }
 
-fn shiptermShell(allocator: std.mem.Allocator) !void {
+fn shiptermShell(allocator: std.mem.Allocator, Galaxy: GalaxyGraph) !void {
     const reader = std.io.getStdIn().reader();
     const writer = std.io.getStdOut().writer();
 
@@ -61,6 +69,9 @@ fn shiptermShell(allocator: std.mem.Allocator) !void {
             \\2. Scan Star
             \\3. Scan Planet
             \\4. Quit Nav System
+            // target star
+            // target Planet
+            // ship diagnostics
         ;
 
         const promptMain = "shipTerm Main > ";
@@ -80,9 +91,9 @@ fn shiptermShell(allocator: std.mem.Allocator) !void {
             const parsedIn = try std.fmt.parseInt(u8, input, 10);
             switch (parsedIn) {
                 1 => try writer.writeAll("Option 1\n\n"),
-                2 => try writer.writeAll("Option 2\n\n"),
+                2 => try scanStar(allocator, writer, reader, Galaxy),
                 3 => try writer.writeAll("Option 3\n\n"),
-                4 => try writer.writeAll("Option 4\n\n"),
+                4 => goodBye(),
                 else => try writer.writeAll("Not an Option\n\n"),
             }
         }
@@ -95,4 +106,29 @@ fn trimWindowsReturn(buffer: []u8) ![]const u8 {
     } else {
         return buffer;
     }
+}
+fn scanStar(alloc: std.mem.Allocator, writer: anytype, reader: anytype, galaxy: GalaxyGraph) !void {
+    try writer.writeAll("What's the name of the star?: ");
+
+    const userInput: []u8 = try reader.readUntilDelimiterAlloc(alloc, '\n', 512);
+    try writer.writeAll("\n");
+    defer alloc.free(userInput);
+    if (@TypeOf(userInput) == []u8) {
+        const processed = try trimWindowsReturn(userInput);
+        const hmm = galaxy.nodes.getEntry(processed);
+        if (hmm == null) {
+            std.debug.print("Star not found in our database. Remember Capitalization is Important\n ", .{});
+            return;
+        }
+        const answer = galaxy.nodes.getPtr(processed);
+        try writer.print("{}\n", .{answer.?.*});
+        try writer.print("ENTER TO CONTINUE\n", .{});
+        _ = try reader.readUntilDelimiterAlloc(alloc, '\n', 512);
+    }
+    return;
+}
+fn goodBye() void {
+    std.debug.print("Logging off...\n", .{});
+    std.debug.print("Goodbye!\n", .{});
+    std.process.exit(0);
 }
